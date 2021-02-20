38676e2fce48b1e3bd02e1521c65b139236c2dda
Steven Berdak <stevenberdak@gmail.com>
2021-02-19 21:54:23 -0800

    auto-commit

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -3,7 +3,7 @@ const chromium = require('chrome-aws-lambda');
 
 const INARA_ROOT = "https://inara.cz/market/"
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
 
     const { station, system } = event.queryStringParameters
 
@@ -14,36 +14,84 @@ exports.handler = async function(event, context) {
         }
     }
 
-    return {
-        statusCode: 200,
-        body: buildURLPath(system, station)
+    let data = 'No Data'
+
+    try {
+        // 1. Launch a new browser 
+        const browser = await puppeteer.launch({
+            // Required
+            executablePath: await chromium.executablePath,
+
+            // Optional
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            headless: chromium.headless
+        });
+
+        // 2. Open a new page
+        const page = await browser.newPage();
+
+        // 3. Navigate to the given URL
+        await page.goto(buildURLPath(system, station));
+
+        data = await page.evaluate(() => {
+
+            // document.querySelector('#ui-id-4').click()
+
+            // await Timeout.set(getBoundedRandomDelay())
+
+            const nodeList = document.querySelectorAll('.mainblock.maintable')
+
+            // Node list length is 3 if market not found (station/system not found)
+            if (nodeList.length === 3) {
+                return ({ data: 'System/Station Not Found' })
+            }
+
+            const marketDiv = nodeList[3]
+
+            const tableBody = marketDiv.children[0].tBodies[0]
+
+            const rows = tableBody.rows
+
+            const results = []
+            
+            rows.forEach(row => {
+                let name, sellPrice, supply
+
+                if (row.children[0].classList.contains('subheader')) {
+                    return
+                } else {
+                    name = rows[1].children[0].children[0].innerText
+
+                    sellPrice = rows[1].children[1].innerText
+
+                    supply = rows[1].children[4].innerText
+
+                    if (supply === '-') {
+                        return
+                    }
+
+                    //const marketRow = new MarketRow(name, sellPrice, supply)
+
+                    results.push({ name, sellPrice, supply })
+                }
+            })
+
+            return results
+        });
+
+        await browser.close();
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: 'Server Error: \n' + e
+        }
     }
 
-    // // 1. Launch a new browser 
-    // const browser = await puppeteer.launch({
-    //     // Required
-    //     executablePath: await chromium.executablePath,
-
-    //     // Optional
-    //     args: chromium.args,
-    //     defaultViewport: chromium.defaultViewport,
-    //     headless: chromium.headless
-    // });
-
-    // // 2. Open a new page
-    // const page = await browser.newPage();
-
-    // // 3. Navigate to the given URL
-    // await page.goto(buildURLPath(system, station));
-
-    // const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-
-    // await browser.close();
-
-    // return {
-    //     statusCode: 200,
-    //     body: data
-    // };
+    return {
+        statusCode: 200,
+        body: data
+    };
 }
 
 function buildURLPath(system, station) {
@@ -51,4 +99,13 @@ function buildURLPath(system, station) {
     let query = `?ps1=${system.replace(' ', '+')}+%5B${station.replace(' ', '+')}%5D`
 
     return INARA_ROOT + query
+}
+
+class MarketRow {
+
+    constructor(name, sellPrice, supply) {
+        this.name = name
+        this.sellPrice = sellPrice
+        this.supply = supply
+    }
 }
