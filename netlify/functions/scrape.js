8108c2dcc67e6ad8a2af03a89c635fb405dc4a71
Steven Berdak <stevenberdak@gmail.com>
2021-02-19 20:51:57 -0800

    auto-commit

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -1,3 +1,30 @@
+const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda');
+
 exports.handler = async function(event, context) {
-    // your server-side functionality
+    // 1. Launch a new browser 
+    const browser = await puppeteer.launch({
+        // Required
+        executablePath: await chromium.executablePath,
+
+        // Optional
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        headless: chromium.headless
+    });
+
+    // 2. Open a new page
+    const page = await browser.newPage();
+
+    // 3. Navigate to the given URL
+    await page.goto('https://google.com');
+
+    const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+
+    await browser.close();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ data: data })
+    };
 }
